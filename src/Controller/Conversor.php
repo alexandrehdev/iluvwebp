c6071d921b3e2a83e3ed5c43b5555893b1bad7ba
alexandrehdev <hallyssonrdev@gmail.com>
2022-09-15 15:11:14 -0300

    redirect to root path after download

--- src/Controller/Conversor.php
+++ src/Controller/Conversor.php
@@ -1,78 +1,71 @@
 <?php
 namespace Conversor\Webp\Controller;
+use Conversor\Webp\Controller\Builder;
 
  class Conversor{                    
+    
+   public function imageData() :array{
+      $builder = new Builder;
+      $image = $builder->getImage();
 
-     public $image;
+      return $image;
+   } 
 
-     function __construct()
-     {
-         $this->setImage($_FILES['image']);
-     }
+   public function convertImage(){
+      $image = $this->imageData();
 
-     public function getImage(){
-         return $this->image;
-     }
+      switch($image["type"]){
+        case "image/png":
+          $this->PngToWebp($image);
+          break;
+        case "image/jpeg":
+          $this->JpgToWebp($image);
+          break;              
+       }
+    }
 
-     public function setImage($image) {
-        $this->image = $image;
-     }
-
-
-     public function convertImage(){
-        $image = $this->getImage();
-
-        switch($image["type"]){
-            case "image/png":
-                $this->PngToWebp($image);
-                break;
-            case "image/jpeg":
-                $this->JpgToWebp($image);
-                break;              
-        }
-     }
+   public function moveImage(array $img, string $type){
+      $target = "resources/img/{$type}/{$img['name']}";
+      move_uploaded_file($img['tmp_name'], $target);
+   }
 
 
-     public function moveImage(array $img, string $type){
-         $target = "resources/img/{$type}/{$img['name']}";
-         move_uploaded_file($img['tmp_name'], $target);
-     }
-
-
-     public function JpgToWebp($img) :void{
-         $image = $this->getImage();
-         $this->moveImage($image, "jpeg");
-         $imagePath = "resources/img/jpeg/{$img['name']}";
-         $image = imagecreatefromjpeg($imagePath);
-         $file = end(explode("/", $imagePath));
-         $extension = end(explode(".", $file));
-         $newImage = str_replace("." . $extension, ".webp", $imagePath);
-         $quality = 100;
-         $this->downloadLink($newImage);
-         imagewebp($image,$newImage,$quality);
-         unlink($imagePath);
+    public function JpgToWebp($img) :void{
+       $image = $this->imageData();
+       $this->moveImage($image, "jpeg");
+       $imagePath = "resources/img/jpeg/{$img['name']}";
+       $image = imagecreatefromjpeg($imagePath);
+       $file = end(explode("/", $imagePath));
+       $extension = end(explode(".", $file));
+       $newImage = str_replace("." . $extension, ".webp", $imagePath);
+       $quality = 100;
+       $this->downloadLink($newImage);
+       imagewebp($image,$newImage,$quality);
+       unlink($imagePath);
      }
 
      public function PngToWebp($img) :void{
-         $image = $this->getImage();
-         $this->moveImage($image, "png");
-         $imagePath = "resources/img/png/{$img['name']}";
-         $image = imagecreatefrompng($imagePath);
-         $newImage = str_replace(".png",".webp",$imagePath);
-         $quality = 100;
-         $this->downloadLink($newImage);
-         imagewebp($image,$newImage,$quality);
-         unlink($imagePath);
+       $image = $this->imageData();
+       $this->moveImage($image, "png");
+       $imagePath = "resources/img/png/{$img['name']}";
+       $image = imagecreatefrompng($imagePath);
+       $newImage = str_replace(".png",".webp",$imagePath);
+       $quality = 100;
+       $this->downloadLink($newImage);
+       imagewebp($image,$newImage,$quality);
+       unlink($imagePath);
      }                         
 
      public function downloadLink(string $pathDownload) :void{
          $downloadHtml = "resources/elements/download.html";
 
-         $content = "<div id='download-area'>
+         $content = "
+                    <div id='download-area'>
                         <a name='downloadBtn'id='btnDown'href='{$pathDownload}'download>
                             Download Image <i class='fa-solid fa-download'></i>
                        </a>
-                    </div>";
+                    </div>
+                    ";
 
          file_put_contents($downloadHtml,$content);
      }
